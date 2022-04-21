5368f80c25a7ff448ca305d17f8ee6a82097ad44
thetime50 <1004763777@qq.com>
2022-04-21 08:29:45 +0800

    debug

--- code_no_translate.js
+++ code_no_translate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         code-no-translate
 // @namespace    http://thetime50.com/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       thetime50
 // @match        https://pyimagesearch.com/*
@@ -12,21 +12,32 @@
 // @updateURL    https://thetime50.github.io/tampermonkeyscript/code_no_translate.js
 // ==/UserScript==
 
-(function() {
+function delay(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+(async function() {
     'use strict';
     let classList = []
     // https://pyimagesearch.com/
     if(location.host.indexOf('pyimagesearch')){
+        await delay(2000)
         classList = ['.enlighter-codegroup-wrapper']
+        console.log('pyimagesearch', classList)
     }
     // https://github.com/
     if(location.host.indexOf('github')){
         // js-blob-code-container blob-code-content
+        await delay(2000)
         classList = ['table[data-tagsearch-lang]','[class*=highlight-source-]']
+        console.log('github', classList)
     }
     if(classList.length > 0){
         classList.forEach((v,i,a)=>{
-            $(v).attr( "translate","no")
+            let domList = $(v).attr( "translate","no")
+            console.log('domList', domList)
         })
     }
 })();
