6bc8bb794df2ea9691b39e35296aae8b3cde2af4
thetime50 <1004763777@qq.com>
2022-04-21 08:03:26 +0800

    code-no-translate

---
+++ code_no_translate.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         code-no-translate
+// @namespace    http://thetime50.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       thetime50
+// @match        https://pyimagesearch.com/*
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pyimagesearch.com
+// @grant        none
+// @require      http://libs.baidu.com/jquery/1.7.2/jquery.min.js
+// @updateURL    https://thetime50.github.io/tampermonkeyscript/code_no_translate.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let classList = []
+    // https://pyimagesearch.com/
+    if(location.host.indexOf('pyimagesearch')){
+        classList = ['enlighter-toolbar-bottom','[class*=highlight-source-]']
+    }
+    // https://github.com/
+    if(location.host.indexOf('github')){
+        // js-blob-code-container blob-code-content
+        classList = ['table[data-tagsearch-lang]']
+    }
+    if(classList.length > 0){
+        classList.forEach((v,i,a)=>{
+            $(v).attr( "translate","no")
+        })
+    }
+})();
