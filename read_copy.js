1b7d4b7d5263fd669a5a59634c134f5cccf74d88
thetime50 <1004763777@qq.com>
2026-06-18 03:15:44 +0800

    feat: 新浪财经 自动关闭窗口 间隔0.3s循环执行

--- read_copy.js
+++ read_copy.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         read_copy
 // @namespace    http://thetime50.com/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.bilibili.com/read/*
 // @match        https://blog.csdn.net/*
+// @match        https://finance.sina.com.cn/realstock/company/*
 // @grant        none
 // @require      http://libs.baidu.com/jquery/1.7.2/jquery.min.js
 // @updateURL    https://thetime50.github.io/tampermonkeyscript/read_copy.js
@@ -17,19 +18,6 @@ function delay(ms) {
 }
 (async function() {
     'use strict';
-
-    const cfgList=[
-        {
-            hostname: 'bilibili.com',
-            selector: ['.unable-reprint'],
-        }, {
-            hostname: 'blog.csdn.net',
-            selector: [
-                '.prettyprint', '.prettyprint>code',
-                'pre', 'pre>code',
-            ],
-        }
-    ]
 
     function getStyle(selector){
         let content = selector.map((v,i,a)=>{
@@ -58,7 +46,6 @@ function delay(ms) {
         parent.replaceChild(copy, node)
     }
 
-    await delay(2000)
 
     console.log("read-copy 2333")
     // $(document).unbind("copy")
@@ -68,16 +55,64 @@ function delay(ms) {
     //     el.oncopy=()=>{}
     // })
     // console.log(window.getEventListeners(document))
-    let cfg = cfgList.find(v=>{
-        return window.location.hostname.indexOf(v.hostname)>-1
-    })
-    if (cfg) {
-        $("html").append(getStyle(cfg.selector));
 
-        $(cfg.selector.join(',')).each((i, el) => {
+    async function cfgListExec(list){
+        
+        let cfg = list.find(v=>{
+            if(v.hostname){
+                return window.location.hostname.indexOf(v.hostname)>-1
+            }
+            if(v.re){
+                return v.re.test(window.location.href)
+            }
+            return false
+        })
+        if (cfg) {
+            try{
+                await cfg.cb()
+            } catch (e) {
+                console.error('cfgListExec error')
+                console.log(cfg, e)
+            }
+        }
+    }
+
+    
+    async function readToCopy(selector){
+        await delay(2000)
+        $("html").append(getStyle(selector));
+
+        $(selector.join(',')).each((i, el) => {
             removeEventListener(el)
         })
     }
 
+    // 新浪财经 自动关闭窗口 间隔0.3s循环执行
+    function sinaFinanceAutoCloseWindow(){
+        setInterval(()=>{
+            document.querySelectorAll('[class*="close"]').forEach(el=>{
+                if (el.style.display !== 'none' && el.offsetParent !== null) {
+                    console.log('click', el)
+                    el.click()
+                }
+            })
+        }, 300)
+    }
+
+
+    const cfgList=[
+        {
+            hostname: 'bilibili.com',
+            cb: ()=>readToCopy(['.unable-reprint']),
+        }, {
+            hostname: 'blog.csdn.net',
+            cb: ()=>readToCopy(['.prettyprint', '.prettyprint>code','pre', 'pre>code']),
+        }, {
+            re:/\/\/finance.sina.com.cn\/realstock\/company\/\w+\/nc.shtml/,
+            cb: sinaFinanceAutoCloseWindow,
+        }
+    ]
+
+    cfgListExec(cfgList)
     // Your code here...
 })();
