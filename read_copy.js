6b3e895fdbf1f442e9b4e57ce6ac37fb3136ef75
thetime50 <1004763777@qq.com>
2026-06-18 05:23:08 +0800

    feat: 添加对比搜索记录

--- read_copy.js
+++ read_copy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         read_copy
 // @namespace    http://thetime50.com/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.bilibili.com/read/*
@@ -16,6 +16,18 @@ function delay(ms) {
         setTimeout(resolve, ms);
     })
 }
+
+function waitElement(selector){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let el = document.querySelector(selector)
+            if (el) {
+                resolve(el)
+            }
+        }, 100)
+    })
+}
+
 (async function() {
     'use strict';
 
@@ -87,15 +99,79 @@ function delay(ms) {
         })
     }
 
-    // 新浪财经 自动关闭窗口 间隔0.3s循环执行
-    function sinaFinanceAutoCloseWindow(){
+    // 新浪财经 
+    async function sinaFinanceAutoCloseWindow(){
+        let symbolIds = JSON.parse(localStorage.getItem('symbolIds')) || []
+
+        let compareIndexH5Is = $(await waitElement('#compareIndexH5 .is'))
+        compareIndexH5Is.on('click', 'a[x-symbol]', async function(e) {
+            e.preventDefault()
+            let symbolId = $(this).attr('x-symbol')
+            $('#compareTxtH5').val(symbolId)
+            await delay(10)
+            // $('#compareBtnH5').trigger('click')
+            document.querySelector('#compareBtnH5').click()
+        })
+        compareIndexH5Is.on('click', 'label', function(e) {
+            e.preventDefault()
+            let symbolId = $(this).siblings('a').attr('x-symbol')
+            $(this).closest('.i').remove()
+            let idx = symbolIds.findIndex(v => v.id === symbolId)
+            if (idx > -1) {
+                symbolIds.splice(idx, 1)
+                localStorage.setItem('symbolIds', JSON.stringify(symbolIds))
+            }
+        })
+        compareIndexH5Is.css({
+            top: 'unset',
+            bottom: '25px',
+            width: '180px',
+        })
+        function addSymbolItem(symbolId, name){
+            let $item = $('<div class="i" style="display: flex;"></div>')
+                .css({flex: '1'})
+            $item.append($('<a href="javascript:void(0)">')
+                .attr('x-symbol', symbolId).text(name).css({
+                    flex: '1 1 auto',
+                })
+            )
+            $item.append(' ')
+            $item.append($('<label>').text('x').css({
+                flex: '0 0 auto',
+                width: '20px',
+                height: '20px',
+                textAlign: 'center',
+            }))
+            compareIndexH5Is.append($item)
+        }
+        symbolIds.forEach(v=>{
+            addSymbolItem(v.id, v.name)
+        })
+
         setInterval(()=>{
+            let addNewSymbol = false
+            // 自动关闭窗口 间隔0.3s循环执行
             document.querySelectorAll('[class*="close"]').forEach(el=>{
                 if (el.style.display !== 'none' && el.offsetParent !== null) {
-                    console.log('click', el)
+                    // console.log('click', el)
                     el.click()
                 }
             })
+            /* 如果selectAll #h5CompareCon>[data-symbol] 有值 去重加入symbolIds数组
+            */
+            let symbolEls = document.querySelectorAll('#h5CompareCon>[data-symbol]')
+            symbolEls.forEach(v=>{
+                let symbolId = v.getAttribute('data-symbol')
+                if (!symbolIds.some(v => v.id === symbolId)) {
+                    let name = $(v).text().trim() || symbolId
+                    symbolIds.push({id: symbolId, name})
+                    addSymbolItem(symbolId, name)
+                    addNewSymbol = true
+                }
+            })
+            if (addNewSymbol) {
+                localStorage.setItem('symbolIds', JSON.stringify(symbolIds))
+            }
         }, 300)
     }
 
